1cce0c6bec0e99a685ebe3f2a90d85fe8efbbaed
Krzysztof Rozbicki <s217259@sggw.edu.pl>
2023-06-20 21:03:37 +0200

    Add GameGrid class description

--- src/SGGW.Tetris-Project/GameGrid.cs
+++ src/SGGW.Tetris-Project/GameGrid.cs
@@ -8,5 +8,21 @@ namespace SGGW.Tetris_Project
 {
     public class GameGrid
     {
+        //TODO:
+
+        //pola:
+        //private int[,] siatka 
+        //public get po kolumnach i get po rzedach (mozna zrobic jako indekser)
+        //
+        //constructor (rzedy+2 , kolumny) on okresla siatke
+
+        //metody:
+        //public bool IsInside(wspolrzedna rzad, kolumna) zwraca tak czy nie
+        //public bool IsRowFull(nr rzedu) tak czy nie
+        //public bool IsRowEmpty(nr rzedu) tak czy nie
+        //private void ClerRow(nr_rzedu) usuwamy wszystkie wartosci z rzedu (ustawiamy na 0)
+        //private void MoveRowDown(nr_rzedu,ilosc_spadkow) przesowa gorne rzedy o dana ilosc w dol 
+        //public int ClearFullRow(): return ilosc_pustych_rzedow
+
     }
 }
