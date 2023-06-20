4603b33306df0fc3a0070b262f5d748066c7857b
Krzysztof Rozbicki <s217259@sggw.edu.pl>
2023-06-20 21:28:48 +0200

    Add BlockQueue class description

--- src/SGGW.Tetris-Project/BlockQueue.cs
+++ src/SGGW.Tetris-Project/BlockQueue.cs
@@ -8,5 +8,15 @@ namespace SGGW.Tetris_Project
 {
     public class BlockQueue
     {
+        //TODO:
+        //pola
+        //Block[] blocks (zawiera 7 klas blokow I, J, L, O, T, S ,Z)
+        //Random do generowania
+        
+        //metody
+        //private Block RandomBlock()
+        //public  Block NexteBlock()
+        //public Block GetAndUpdate() daje kolejny blok i uaktualnia kolejke
+        //
     }
 }
