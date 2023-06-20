1b7753dc746c42f6a6026e7b5158242043c2fb77
Krzysztof Rozbicki <s217259@sggw.edu.pl>
2023-06-20 21:22:09 +0200

    Add Block class description

--- src/SGGW.Tetris-Project/Block.cs
+++ src/SGGW.Tetris-Project/Block.cs
@@ -8,6 +8,21 @@ namespace SGGW.Tetris_Project
 {
     abstract class Block
     {
+        //TODO:
+        //pola:
+        //abstarct Postions[][] Tiles
+        //abstarc Position Start 
+        //abstarct int ID
+        //abstract int rotationState
+        //Position Offset
+        //public Constructor (Start Offset Column i ROW) w konkretnych blokach dodaje Tiles
+
+        //metody all public
+        // IEnumerable<Positions> TilePositions() return lista aktualnych pozycji bloku
+        //void RotateL() obraca w lewo
+        //void RotateR() obraca w prawo
+        //void Move(rzad,kolumna) rusza w prawo lewo i dol
+        //??? void Reset() zmiana do pierwotnej pozycji
 
     }
 }
