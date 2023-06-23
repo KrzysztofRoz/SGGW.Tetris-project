865ab659139f17340d540aa4e65fa2a2b63bd5c7
Krzysztof Rozbicki <s217259@sggw.edu.pl>
2023-06-23 19:55:42 +0200

    Dodano implementacje klas wszystkich blokow

---
+++ src/SGGW.Tetris-Project/JBlock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGGW.Tetris_Project
+{
+    public class JBlock : Block
+    {        
+        private readonly Positions[][] tiles =  new Positions[][]
+        {
+            new Positions[] {new Positions(0,0), new Positions(1,0), new Positions(1,1), new Positions(1,2)},
+            new Positions[] {new Positions(0,1), new Positions(0,2), new Positions(1,1), new Positions(2,1)},
+            new Positions[] {new Positions(1,0), new Positions(1,1), new Positions(1,2), new Positions(2,2)},
+            new Positions[] {new Positions(0,1), new Positions(1,1), new Positions(2,0), new Positions(2,1)}
+        };
+        public override int Id => 2;
+
+        protected override Positions StartOffset => new Positions(0,3);
+        protected override Positions[][] Tiles => tiles;
+
+    }
+}
